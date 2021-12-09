4a1bf5b8c884f5fa6c1e837f2e9245895681215c
沈晨玙 <939826879@qq.com>
2021-12-09 17:25:21 +0800

    2

--- src/main/java/jwt/JWTUtil.java
+++ src/main/java/jwt/JWTUtil.java
@@ -9,40 +9,34 @@ import java.util.Date;
 
 public class JWTUtil {
     //设置密钥
-    private static String key="kappy";
+    private static String key = "kappy";
     //有效时间长度  60分钟
-    private static Long failTime=1000*60*30L;
+    private static Long failTime = 1000 * 60 * 60L;
 
     /**
      * 生成token
      */
-    public static String createJsonWebToken(UserInfoVo user){
-       //判断用户对象信息
-        if(user==null || user.getId()==null || user.getUsername()==null){
+    public static String createJsonWebToken(UserInfoVo user) {
+        //判断用户对象信息
+        if (user == null || user.getId() == null || user.getUsername() == null) {
             return null;
         }
 
         //生成token对象
-        String token= Jwts.builder()
-                .claim("id",user.getId())
-                .claim("username",user.getUsername())
-                .claim("type",user.getType())
-                .setIssuedAt(new Date())//签发时间
-                .setExpiration(new  Date(System.currentTimeMillis()+failTime))
-                .signWith(SignatureAlgorithm.HS256,key)
-                .compact();
+        String token = Jwts.builder().claim("id", user.getId()).claim("username", user.getUsername()).claim("type", user.getType()).setIssuedAt(new Date())//签发时间
+                .setExpiration(new Date(System.currentTimeMillis() + failTime)).signWith(SignatureAlgorithm.HS256, key).compact();
 
-         return token;
+        return token;
     }
 
     /**
      * 验证token是否有效
      */
-    public static Claims checkJwt(String token){
+    public static Claims checkJwt(String token) {
         try {
             Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
             return claims;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -51,7 +45,7 @@ public class JWTUtil {
     /**
      * 获取用户id
      */
-    public static Integer getUserId(String token){
+    public static Integer getUserId(String token) {
         Claims claims = JWTUtil.checkJwt(token);
         return (Integer) claims.get("id");
     }
@@ -59,7 +53,7 @@ public class JWTUtil {
     /**
      * 获取用户姓名
      */
-    public static String getUsername(String token){
+    public static String getUsername(String token) {
         Claims claims = JWTUtil.checkJwt(token);
         return (String) claims.get("username");
     }
@@ -67,7 +61,7 @@ public class JWTUtil {
     /**
      * 获取用户姓名
      */
-    public static String getUserType(String token){
+    public static String getUserType(String token) {
         Claims claims = JWTUtil.checkJwt(token);
         return (String) claims.get("type");
     }
@@ -75,27 +69,27 @@ public class JWTUtil {
     /**
      * 判断token是否过期
      */
-    public static boolean isTokenExpired(String token){
+    public static boolean isTokenExpired(String token) {
         Claims claims = JWTUtil.checkJwt(token);
-        Date times=claims.getExpiration();
+        Date times = claims.getExpiration();
         return times.before(new Date());
     }
 
-    public static void main(String [] args){
-       UserInfoVo user=new UserInfoVo();
+    public static void main(String[] args) {
+        UserInfoVo user = new UserInfoVo();
         user.setId(1);
         user.setType("1");
         user.setUsername("kappys");
-        String token=createJsonWebToken(user);
-        System.out.println("token:"+token);
+        String token = createJsonWebToken(user);
+        System.out.println("token:" + token);
 
         //解析用户名称信息
-        String name= getUsername(token);
-        System.out.println(name+"---------");
+        String name = getUsername(token);
+        System.out.println(name + "---------");
 
         //String string="eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJrYXBweSIsInR5cGUiOiIxIiwiaWF0IjoxNjEyMzY2NzY5LCJleHAiOjE2MTIzNjg1Njl9.CeLztv0LVZuKwxNHEuNKNQxYC-hxsOIfF99fgORsgok";
-        boolean bs=isTokenExpired(token);
-        System.out.println(bs+"~~~~");
+        boolean bs = isTokenExpired(token);
+        System.out.println(bs + "~~~~");
 
 
     }
